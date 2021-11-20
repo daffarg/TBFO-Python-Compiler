a7b0560b65fd1f1d803d32a8b8131061a6b83811
Mohamad Daffa Argakoesoemah <argakoesoemahmdaffa@gmail.com>
2021-11-20 18:13:42 +0700

    add read py file function & fix cfg

---
+++ tes.py
@@ -0,0 +1,2 @@
+a = "5.6"
+print(float(a))
--- tokens.py
+++ tokens.py
@@ -1,32 +1,29 @@
-BLANK = " \t"
+BLANK = " "
+TAB = "\t"
+MARK = "\n"
 
 def readPythonFile(filepath):
     f = open(filepath,'r')
-    lines = f.readlines()
-    return lines
-
-# def readline(line):
-
-def ignoreBlank(cc,idx,line):
-    if cc in BLANK:
-        idx = idx+1
-        cc = line[idx]
-
-def adv(cc,idx,line):
-    ignoreBlank(cc,idx,line)
-    string = ""
-    while cc not in (BLANK+'\n'):
-        string += cc
-        idx += 1
-        cc = line[idx]
-    return string
-
-filepath = input()
-# print(readPythonFile(filepath))
-lines = readPythonFile(filepath)
-for line in lines:
-    idx = 0
-    cc = line[idx]
-    while cc != "\n":
-        string = adv(cc,idx,line)
-        print(string) 
+    lines  = f.readlines()
+    array_of_codes = []
+    for line in lines:
+        array_of_strings = []
+        idx = 0
+        while line[idx] == BLANK or line[idx] == TAB:
+            idx += 1
+        if line[idx] == MARK:
+            f.close()
+        else:   
+            while True:
+                string = ""
+                while line[idx] != BLANK and line[idx] != TAB and line[idx] != MARK:
+                    string += line[idx]
+                    idx += 1
+                array_of_strings.append(string)
+                if line[idx] == MARK or idx > len(line) - 1:
+                    break
+                while line[idx] == BLANK or line[idx] == TAB:
+                    idx += 1
+        array_of_codes.append(array_of_strings)
+    return array_of_codes
+print(readPythonFile("tes.txt"))
